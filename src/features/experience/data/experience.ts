f0e1e4506b3c349e0a8b64062c97e8ac47f43d98
Ezequiel <132577882+ezequiel-dev93@users.noreply.github.com>
2025-10-21 19:48:56 +0200

    Se añadio experience

--- src/features/experience/data/experience.ts
+++ src/features/experience/data/experience.ts
@@ -7,13 +7,13 @@ export const experience: Experience[] = [
     title: "Desarrollador Front-End",
     company: "Freelancer",
     description: [
-      "Desarrollé un sitio web para un músico que <span>facilité</span> la <span>conexión</span> con su audencia y su <span>presencia</span>.",
+      "Desarrollé un sitio web para un músico que <span>facilité</span> la <span>conexión</span> con su audiencia y su <span>presencia</span>.",
 
-      "Optimecé el <span>SEO</span>, alcanzando 100% de <span>rendimiento</span>, 96% en <span>perfomance</span> y un 94% en <span>accesibilidad</span> (Lighthouse).",
+      "Optimicé el <span>SEO</span>, alcanzando 100% de <span>rendimiento</span>, 96% en <span>performance</span> y un 94% en <span>accesibilidad</span> (Lighthouse).",
 
-      "Diseñé una <span>experiancia</span> de <span>usuario</span> accesible y <span>responsiva</span>, que permitea los usuarios navegar de forma clara e intuitiva.",
+      "Diseñé una <span>experiancia</span> de <span>usuario</span> accesible y <span>responsiva</span>, que permite a los usuarios navegar de forma clara e intuitiva.",
 
-      "Implementé <span>screaming architecture</span> para garantizar <span>escabilidad</span> que facilite la incorporación de nuevas <span>funcionalidades</span> en el futuro, <span>sin afectar al sistema</span>.",
+      "Implementé <span>screaming architecture</span> para garantizar <span>escalabilidad</span> que facilite la incorporación de nuevas <span>funcionalidades</span> en el futuro, <span>sin afectar al sistema</span>.",
     ],
     skills: ["Astro", "Tailwind", "TypeScript", "ReactJs", "Web Responsive"],
   }
